4e13ff321a317bf9d732682f8f38174226fcefae
Oswaldo G <oswaldo.gg0520@gmail.com>
2022-05-01 12:56:09 -0500

    Change folder root of the proyect

--- Backend/helpers/password.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const hashPass = async pass => await bcrypt.hash(pass, 5);
-const comparePass = async (pass, hash) => await bcrypt.compare(pass, hash);
-
-module.exports = {
-  hashPass,
-  comparePass,
-};
